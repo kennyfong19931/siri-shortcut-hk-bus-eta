0dfc70cc6f1fd1678b20e97e2b5945f1004d91cf
kennyfong19931 <kennyfong19931@gmail.com>
2022-08-07 17:53:42 +0800

    increase retry timeout

--- src/updateRoute.ts
+++ src/updateRoute.ts
@@ -41,7 +41,7 @@ const doRequest = async (method: string, url: string, body?: {}, toString = fals
         if (result !== null && result !== undefined)
             return result;
 
-        await new Promise(r => setTimeout(r, 60000));
+        await new Promise(r => setTimeout(r, 120000));
     }
 }
 
