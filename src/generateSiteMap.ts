c8a2061f2f36a4243cd9f2f40ff3cc92af155cd1
kennyfong19931 <kennyfong19931@gmail.com>
2024-06-07 18:29:28 +0800

    fix GMB

--- src/generateSiteMap.ts
+++ src/generateSiteMap.ts
@@ -1,12 +1,23 @@
 import fs from 'fs';
 import path from 'path';
 
-import { COMPANY } from './constant';
 import logger from './utils/logger';
 
-const DOMAIN = 'https://siri-shortcut-hk-bus-eta.pages.dev/';
+const DOMAIN = 'https://siri-shortcut-hk-bus-eta.pages.dev';
 const routeFolder = path.join('public', 'api', 'route');
 const sitemapFile = path.join('public', 'sitemap.txt');
+
+function getRouteUrl(data) {
+    if ('mtr_hr' === data.company) {
+        return `/${data.company}/${data.routeId}`;
+    } else if ('nlb' === data.company) {
+        return `/${data.company}/${data.route}/${data.routeId}`;
+    } else if ('gmb' === data.company) {
+        return `/${data.company}/${data.route}/${data.routeType}`;
+    } else {
+        return `/${data.company}/${data.route}/${data.dir}`;
+    }
+}
 
 (async function () {
     logger.info('Start');
@@ -19,21 +30,7 @@ const sitemapFile = path.join('public', 'sitemap.txt');
         const data = fs.readFileSync(filePath, 'utf8');
         const routeList = JSON.parse(data);
         for (let route of routeList) {
-            let url = '';
-            switch(route.company) {
-                case COMPANY.MTR_HR.CODE:
-                   url = `${DOMAIN}${route.company}/${route.routeId}`;
-                    break;
-                case COMPANY.NLB.CODE:
-                case COMPANY.GMB.CODE:
-                    url = `${DOMAIN}${route.company}/${route.route}/${route.routeId}`;
-                    break;
-                default:
-                    url = `${DOMAIN}${route.company}/${route.route}/${route.dir}`;
-                    break;
-            }
-            //write string to sitemap.txt
-            fs.appendFileSync(sitemapFile, `${url}\n`, 'utf8');
+            fs.appendFileSync(sitemapFile, `${DOMAIN}${getRouteUrl(route)}\n`, 'utf8');
             count++;
         }
     }
--- src/js/route.js
+++ src/js/route.js
@@ -17,7 +17,6 @@ window.addEventListener('load', () => {
                 /kmb/:route/:dir
                 /ctb/:route/:dir
                 /nlb/:route/:routeId
-                /gmb/:route/:routeId
                 /mtr/:route/:dir
                 /mtr_hr/:routeId/:stop
                 /mtr_lr/:routeId/:dir
@@ -26,7 +25,7 @@ window.addEventListener('load', () => {
             if ('mtr_hr' === params.data.company) {
                 param.routeId = params.data.route;
                 param.stop = params.data.param1;
-            } else if ('nlb' === params.data.company || 'gmb' === params.data.company) {
+            } else if ('nlb' === params.data.company) {
                 param.route = params.data.route;
                 param.routeId = params.data.param1;
             } else {
@@ -44,21 +43,38 @@ window.addEventListener('load', () => {
                 /kmb/:route/:dir/:stop
                 /ctb/:route/:dir/:stop
                 /nlb/:route/:routeId/:stop
-                /gmb/:route/:routeId/:stop
+                /gmb/:route/:routeId/:routeType
                 /mtr/:route/:dir/:stop
                 /mtr_lr/:routeId/:dir/:stop
              */
             let param = {};
-            if ('nlb' === params.data.company || 'gmb' === params.data.company) {
+            if ('nlb' === params.data.company) {
                 param.routeId = params.data.param1;
+                param.stop = params.data.stop;
+            } else if ('gmb' === params.data.company) {
+                param.routeId = params.data.param1;
+                param.routeType = params.data.stop;
             } else {
                 param.dir = params.data.param1;
+                param.stop = params.data.stop;
             }
             handleRoute({
                 company: params.data.company,
                 route: params.data.route,
-                stop: params.data.stop,
                 ...param,
+            });
+        })
+        .on('/:company/:route/:routeId/:routeType/:stop', function (params, query) {
+            /*
+                Possible routes:
+                /gmb/:route/:routeId/:routeType/:stop
+             */
+            handleRoute({
+                company: params.data.company,
+                route: params.data.route,
+                routeId: params.data.routeId,
+                routeType: params.data.routeType,
+                stop: params.data.stop,
             });
         })
         .resolve();
@@ -72,7 +88,8 @@ function handleRoute(inputData) {
                 (element) =>
                     element.company === (inputData.company === 'nwfb' ? 'ctb' : inputData.company) &&
                     (inputData.dir ? element.dir === inputData.dir : true) &&
-                    (inputData.routeId ? element.routeId === inputData.routeId : true),
+                    (inputData.routeId ? element.routeId === inputData.routeId || element.routeId === parseInt(inputData.routeId) : true) &&
+                    (inputData.routeType ? element.routeType === parseInt(inputData.routeType) : true),
             );
             if (!routeArray) {
                 alert(`Cannot find route ${inputData.route} !`, 'danger');
@@ -80,7 +97,7 @@ function handleRoute(inputData) {
             }
             renderRoute(routeArray[0]);
             if (inputData.stop) {
-                triggerStopClick(inputData.stop);
+                triggerStopClick(inputData.company === 'gmb' ? parseInt(inputData.stop) : inputData.stop);
             }
         })
         .catch(function (error) {
@@ -92,8 +109,10 @@ function handleRoute(inputData) {
 function getRouteUrl(data, withStop = false) {
     if ('mtr_hr' === data.company) {
         return `/${data.company}/${data.routeId}${withStop ? '/' + data.stop : ''}`;
-    } else if ('nlb' === data.company || 'gmb' === data.company) {
+    } else if ('nlb' === data.company) {
         return `/${data.company}/${data.route}/${data.routeId}${withStop ? '/' + data.stop : ''}`;
+    } else if ('gmb' === data.company) {
+        return `/${data.company}/${data.route}/${data.routeId}/${data.routeType}${withStop ? '/' + data.stop : ''}`;
     } else {
         return `/${data.company}/${data.route}/${data.dir}${withStop ? '/' + data.stop : ''}`;
     }
